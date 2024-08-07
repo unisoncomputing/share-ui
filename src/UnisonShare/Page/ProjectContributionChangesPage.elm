5c434dc53cc4c341317e7ab7834f4c02b7e17f58
Simon Højberg <r.hackr@gmail.com>
2024-08-07 16:07:33 -0400

    Hide error

--- src/UnisonShare/Page/ProjectContributionChangesPage.elm
+++ src/UnisonShare/Page/ProjectContributionChangesPage.elm
@@ -405,10 +405,11 @@ viewNamespaceLine projectRef changedDefinitions { name, lines } =
 
 
 viewFailedToLoadExpandedContent : Http.Error -> Html msg
-viewFailedToLoadExpandedContent e =
+viewFailedToLoadExpandedContent _ =
     div [ class "error-expanded-content" ]
         [ text "Error, couldn't load diff details"
-        , div [] [ text (Util.httpErrorToString e) ]
+
+        -- , div [] [ text (Util.httpErrorToString e) ]
         ]
 
 
