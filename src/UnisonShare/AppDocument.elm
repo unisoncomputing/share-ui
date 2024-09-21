a9bfdbacc454d99644c94937d9f1b4a0b7ae5d59
Simon Højberg <r.hackr@gmail.com>
2024-09-21 09:05:51 -0400

    Remove unused functions

--- src/UnisonShare/AppDocument.elm
+++ src/UnisonShare/AppDocument.elm
@@ -1,14 +1,12 @@
 module UnisonShare.AppDocument exposing (AppDocument, appDocument, map, view, withModal)
 
 import Browser exposing (Document)
-import Html exposing (Html, div, img)
-import Html.Attributes exposing (class, id, src, width)
+import Html exposing (Html, div)
+import Html.Attributes exposing (class, id)
 import Maybe.Extra as MaybeE
 import UI
-import UI.Button as Button
 import UI.PageHeader as PageHeader exposing (PageHeader)
 import UnisonShare.AppHeader as AppHeader exposing (AppHeader, AppHeaderContext)
-import UnisonShare.Link as Link
 
 
 
