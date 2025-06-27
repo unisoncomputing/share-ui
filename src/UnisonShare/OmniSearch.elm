2196a7a9b4a769cfb31481efb464a3acd86bd473
Simon Højberg <r.hackr@gmail.com>
2025-06-27 10:00:52 -0400

    Always limit results to 8

--- src/UnisonShare/OmniSearch.elm
+++ src/UnisonShare/OmniSearch.elm
@@ -1235,7 +1235,7 @@ viewSearchSheet appContext viewMatch search =
 
             else
                 div [ class "main-result-sheet" ]
-                    (SearchResults.mapToList viewMatch matches)
+                    (matches |> SearchResults.take 8 |> SearchResults.mapToList viewMatch)
     in
     case search of
         NotAsked _ ->
