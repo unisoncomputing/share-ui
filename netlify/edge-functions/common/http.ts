a4317b4302118a1c6891eb4ffb111a5bcc1c1042
Simon Højberg <r.hackr@gmail.com>
2025-12-05 12:44:12 -0500

    Reduce edgefunction timeout to 10 sec

--- netlify/edge-functions/common/http.ts
+++ netlify/edge-functions/common/http.ts
@@ -1,4 +1,4 @@
-export async function get(url: string, timeout: number = 30000) {
+export async function get(url: string, timeout: number = 10000) {
   const c = new AbortController();
   const id = setTimeout(() => c.abort(), timeout);
   const res = await fetch(url, { signal: c.signal });
